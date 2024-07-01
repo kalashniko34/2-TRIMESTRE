7520909e36e24759e5afd5310e466a72d1b5b136
kalashniko34 <165661069+kalashniko34@users.noreply.github.com>
2024-07-01 08:55:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const caixaprincipal = document.querySelector(".caixa-pricipal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativas = document.querySelector(".caixa-alternativas");
+const caixaresultado = document.querySelector(".caixa-resultado");
+const textoresultado= document.querySelector(".texto-resultado");
+
+const perguntas = [
+    enunciado: "pergunta 1",
+    alternativas: ["alternativa 1",
+"alternativa 2"]
+],
+
+[enunciado:"pergunta 2",
+alternativas:["alternativa 1",
+"alternativa 2"]
+
+]
